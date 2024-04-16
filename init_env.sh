75dc877f52bbd9204291062e82c6021815c4e78d
Munavar Harris Thottoli <60294955+mharris-t@users.noreply.github.com>
2024-04-16 13:50:47 +0000

    fixing init script

--- init_env.sh
+++ init_env.sh
@@ -2,9 +2,12 @@
 
 echo 'Initializing environment..'
 minikube start
+echo '' 
 
 echo 'Creating namespace..'
 kubectl create namespace sre
+echo ''
+
 
 echo 'Performing deployment..'
 kubectl apply -f upcommerce-deployment.yml -n sre

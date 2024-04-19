68f9e206d7292601ea684dee32e53f36816ea00e
Munavar Harris Thottoli <60294955+mharris-t@users.noreply.github.com>
2024-04-19 13:18:38 +0300

    Update watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -4,11 +4,6 @@ NAMESPACE="sre"
 POD_NAME=$(kubectl get pods -n $NAMESPACE --no-headers -o custom-columns=":metadata.name" | grep 'swype' | head -n 1)
 MAX_RESTART_COUNT=5
 
-# Fetch the restart count using kubectl and jq to parse the JSON output
-#RESTART_COUNT=$(kubectl get pod $POD_NAME -n $NAMESPACE -o json | jq '.status.containerStatuses[0].restartCount')
-
-#echo "The restart count of the pod $POD_NAME is: $RESTART_COUNT"
-
 while true
 do
   date && echo -e 'Checking pod restarts...\n'
